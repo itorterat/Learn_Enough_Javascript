f51c2adea5e8dc46a5572a5f2af026be4838cda3
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-04-07 19:27:38 +0200

    --- Rewrite the program to use Map and extend the regex because of exercise 4.5.2

--- count.js
+++ count.js
@@ -16,18 +16,18 @@ If this be error and upon me proved,
 I never writ, nor no man ever loved.`;
 
 // Unique words
-let uniques = {};
+let uniques = new Map();
 
 // All words in the text
-let words = sonnet.match(/\w+/g);
+let words = sonnet.match(/[\w']+/g);
 
 // Iterate through `words` and build up an associative array of unique words.
 for (let i = 0; i < words.length; i++) {
   let word = words[i];
-  if (uniques[word]) {
-    uniques[word] += 1;
+  if (uniques.get(word)) {
+    uniques.set(word, uniques.get(word) + 1);
   } else {
-    uniques[word] = 1;
+    uniques.set(word, 1);
   }
 }
 
