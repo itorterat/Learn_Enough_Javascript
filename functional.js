4375e83baf6348d6f018d34c05321a63e387a72f
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-04-08 00:11:18 +0200

    Add a function to filter an array

--- functional.js
+++ functional.js
@@ -29,3 +29,23 @@ function generateUrls(elements) {
     (element) => `https://www.example.com/${urlify(element)}`
   );
 }
+
+// singles: Imperative version
+function imperativeSingles(elements) {
+  let singles = [];
+  elements.forEach(function (element) {
+    if (element.split(/\s+/).length === 1) {
+      singles.push(element);
+    }
+  });
+  return singles;
+}
+
+console.log(imperativeSingles(states));
+
+// singles: Functional version
+function functionalSingles(elements) {
+  return elements.filter((element) => element.split(/\s+/).length === 1);
+}
+
+console.log(functionalSingles(states));
