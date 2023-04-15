7b70e2a3843f2e0f843ddfa6342b31a6275271c0
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-04-15 03:09:36 +0200

    Define a TranslatedPhrase object which inherites from Phrase

--- palindrome.js
+++ palindrome.js
@@ -12,9 +12,26 @@ function Phrase(content) {
     return this.content.toUpperCase();
   };
 
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  };
+
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === reverse(this.processedContent());
   };
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.translation.toLowerCase();
+  };
+}
+
+TranslatedPhrase.prototype = new Phrase();
